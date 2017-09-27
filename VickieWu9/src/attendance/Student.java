db4d6bb0cae954fba4c16ff65acc73ee3069b5a3
Vicky Icky <Vicky Icky@VickyIcky>
2017-09-27 00:32:25 -0400

    s

--- VickieWu9/src/attendance/Student.java
+++ VickieWu9/src/attendance/Student.java
@@ -1,6 +1,67 @@
 package attendance;
 
-public class Student {
+public class Student implements Attendee
+{
 
+	/**In addition to the following methods, please create a class called 
+	 * 'Student' that implements the Attendee interface and include a 
+	 * constructor that takes two parameters: firstName and lastName.
+	 * Use these values to instantiate corresponding fields.
+	 * @return
+	 */
+	public Student()
+	{
+		
+	}
 	
+	private String first;
+	private String last;
+	
+	public students( String firstName, String lastName)
+	{
+		this.first = firstName;
+		this.last = lastName;
+	}
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPresent(boolean present) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean mathces(String first, String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean matches(String last) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getReportString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
