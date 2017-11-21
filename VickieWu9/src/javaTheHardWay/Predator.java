b60b255bdd55564ed78b5ce1ae73aa8a7e3903b1
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-21 14:25:05 -0500

    Predator

--- VickieWu9/src/javaTheHardWay/Predator.java
+++ VickieWu9/src/javaTheHardWay/Predator.java
@@ -1,14 +1,49 @@
 package javaTheHardWay;
 
-public class Predator extends Animal{
+public class Predator extends ReproductionAnimal{
+	
+	public Predator(Habitat matingArea) {
+		super(matingArea);
+		setmaxLitter(3);
+	}
 
-	public Predator() {
-		// TODO Auto-generated constructor stub
+	public Predator(Habitat matingArea, String description, Trait trait1, Trait trait2) {
+		super(matingArea, description, trait1, trait2);
+		setmaxLitter(3);
+	}
+	
+	public static final String[] PREDATORS = {"wolf", "bear", "lion", "bobcat"};
+	
+	public String getName() {
+		return PREDATORS[(int)(Math.random()*PREDATORS.length)];
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Wilderness wilderness = new Wilderness("A Wooded Area", 10);
+		Predator p = new Predator(wilderness);
+		wilderness.addAnimal(p);
+		wilderness.simulate(2);
+	}
+	
+	public ReproductionAnimal getOffSpring(ReproductionAnimal mate) {
+		return new Predator(habitat, getDescription(),
+				Trait.getDominantTrait(getTrait1(), mate.getTrait1()),
+				Trait.getDominantTrait(getTrait2(), mate.getTrait2()));
+				
+	}
+	
+	public boolean canEat() {
+		int attempts = 0;
+		while( attempts <3) {
+			int tIndex = (int)(Math.random()*habitat.getAnimals().length);
+			Animal target = habitat.getAnimals()[tIndex];
+			if(target instanceof Prey) {
+				habitat.removeAnimal(tIndex);
+				return true;
+			}
+			attempts ++;
+		}
+		return false;
 	}
 
 }
